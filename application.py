d06f9627c21c9f0682787534aeff08fba156a689
Justin Capella <justincapella@gmail.com>
2022-07-16 15:20:06 -0700

    Import data_dir from config.py

--- application.py
+++ application.py
@@ -3,7 +3,9 @@ from multiprocessing import Process
 
 from flask import Flask, request, jsonify
 
-data_dir = os.getenv('MOUNT_DIRECTORY', 'data')
+from config import data_dir, logger
+
+from ca import download_pubinfos
 
 application = Flask(__name__)
 
@@ -16,7 +18,6 @@ def index():
 # @application.before_first_request
 @application.route("/setup")
 def setup():
-    from ca import download_pubinfos
     Process(target=download_pubinfos, args=(data_dir,)).start()
     return "Downloading..."
 
